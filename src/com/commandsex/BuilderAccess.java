b0b5ccfdc06ae80864997aaa14470c437a5689da
Keir Nellyer <keir@nellyer.co.uk>
2013-04-02 21:18:46 +0100

    Realised that having add-ons was a bad idea, good bye a days worth of work :(

--- src/com/commandsex/BuilderAccess.java
+++ src/com/commandsex/BuilderAccess.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 /**
  * Functions needed by the Builder
  * This class MUST NOT use/reference anything to do with Bukkit, CraftBukkit or any included libraries
+ * Unless those libraries are included in the Builder jar
  */
 public class BuilderAccess {
 
