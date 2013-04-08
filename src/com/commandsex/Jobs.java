bed4bba9277654e7c1e2284a83a0ca11570d4706
Keir Nellyer <keir@nellyer.co.uk>
2013-04-08 16:45:44 +0100

    Cleanup for JavaDocs

--- src/com/commandsex/Jobs.java
+++ src/com/commandsex/Jobs.java
@@ -1,4 +1,4 @@
-package com.commandsex.api;
+package com.commandsex;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/com/commandsex/helpers/CommandForwarder.java
+++ src/com/commandsex/helpers/CommandForwarder.java
@@ -5,23 +5,20 @@ import com.commandsex.interfaces.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import java.util.HashMap;
-
+/**
+ * Class used to forward commands to the correct command class
+ */
 public class CommandForwarder implements CommandExecutor {
-
-    public static HashMap<String, Long> lastCommandUsage = new HashMap<String, Long>();
 
     /**
      * This method acts as a command forwarder, this class forwards the command to the applicable class
      */
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String cmdAlias, String[] args) {
         if (!Players.checkCommandSpam(sender)){
+            Players.senderUseCommand(sender);
             String commandName = cmd.getName().toLowerCase().replaceFirst("cex_", "");
 
             try {
-                if (!sender.hasPermission("cex.commandspam.bypass")){
-                    lastCommandUsage.put(sender.getName(), System.currentTimeMillis());
-                }
                 Class<?> clazz = Class.forName("com.commandsex.commands.Command_" + commandName);
                 Command command = (Command) clazz.newInstance();
                 Cmd cmdAnno = clazz.getAnnotation(Cmd.class);
