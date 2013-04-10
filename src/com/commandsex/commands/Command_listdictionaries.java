02fde9a8c3275f6b2ba564c1480c08efceecdf03
kezz101 <1millionchances@gmail.com>
2013-04-10 13:59:31 +0100

    Added comments

--- src/com/commandsex/commands/Command_listdictionaries.java
+++ src/com/commandsex/commands/Command_listdictionaries.java
@@ -5,8 +5,13 @@ import com.commandsex.annotations.Builder;
 import com.commandsex.annotations.Cmd;
 import com.commandsex.helpers.WebHelper;
 import com.commandsex.interfaces.Command;
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.command.CommandSender;
 
+/**
+ * ListDictionaries, gets a list of all the dictionaries available to a server
+ * @author kezz101
+ */
 @Builder(name = "listdictionaries", description = "Displays a list of all available online dictionaries", type = "COMMAND")
 @Cmd(command = "listdictionaries", description = "Displays a list of all available online dictionaries")
 public class Command_listdictionaries implements Command {
@@ -14,7 +19,7 @@ public class Command_listdictionaries implements Command {
     @Override
     public boolean run(CommandSender sender, String[] args, String alias) {
         for(WebHelper.Dictionary dict : WebHelper.Dictionary.values())
-            sender.sendMessage(Language.getTranslationForSender(sender, "listDictionaries", dict.name(), dict.getID() + ""));
+            sender.sendMessage(Language.getTranslationForSender(sender, "listDictionaries", WordUtils.capitalize(dict.name().replace("_", " ")), dict.getID() + ""));
         return true;
     }
 
