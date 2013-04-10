f0b344f08badc6bb1886ccec7597fbeb7df0f27c
kezz101 <1millionchances@gmail.com>
2013-04-10 17:43:38 +0100

    Added /feed and /heal

---
+++ src/com/commandsex/commands/Command_heal.java
@@ -0,0 +1,88 @@
+package com.commandsex.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.plugin.PluginManager;
+
+import com.commandsex.Language;
+import com.commandsex.annotations.Builder;
+import com.commandsex.annotations.Cmd;
+import com.commandsex.helpers.Players;
+import com.commandsex.interfaces.Command;
+import com.commandsex.interfaces.EnableJob;
+
+/**
+ * Heal, fills the health bar of specified player
+ * @author Kezz101
+ */
+@Builder(name = "heal", description = "Fills the health bar of specified player", type = "COMMAND")
+@Cmd(command = "heal", description = "Fills the health bar of specified player", usage = "%c% [player | (<all> [permission]) ]")
+public class Command_heal implements Command, EnableJob{
+    private Permission others = new Permission("cex.heal.others", PermissionDefault.OP);
+    private Permission all = new Permission("cex.heal.all", PermissionDefault.OP);
+    private Permission byPerm = new Permission("cex.heal.byPermission", PermissionDefault.OP);
+    
+    @Override
+    public void onEnable(PluginManager pluginManager) {
+        pluginManager.addPermission(others);
+        pluginManager.addPermission(all);
+        pluginManager.addPermission(byPerm);
+    }
+
+    @Override
+    public boolean run(CommandSender sender, String[] args, String alias) {
+        if(!Players.checkIsPlayer(sender, true))
+            return true;
+        
+        if(args == null || args.length == 0) { // Heal self
+            ((Player) sender).setHealth(20);
+            sender.sendMessage(Language.getTranslationForSender(sender, "healSelf"));
+            return true;
+        }
+        
+        if(args.length == 1) { // Heal others
+            if(args[0].equalsIgnoreCase("all")) {
+                if(!Players.hasPermission(sender, all))
+                    return true;
+                int healed = 0;
+                for(Player player : Bukkit.getOnlinePlayers()) {
+                    player.setHealth(20);
+                    player.sendMessage(Language.getTranslationForSender(sender, "healedBy", sender.getName()));
+                    healed++;
+                }
+                sender.sendMessage(Language.getTranslationForSender(sender, "healOthers", healed + ""));
+            } else {
+                if(!Players.hasPermission(sender, others))
+                    return true;
+                Player target = Players.getPlayer(args[0], sender);
+                if(target != null) {
+                    target.setHealth(20);
+                    target.sendMessage(Language.getTranslationForSender(sender, "healedBy", sender.getName()));
+                    sender.sendMessage(Language.getTranslationForSender(sender, "healOther", target.getName()));
+                }
+            }
+            return true;
+        }
+        
+        if(args.length == 2 && Players.hasPermission(sender, byPerm)) { // Heal by perm
+            if(!args[0].equalsIgnoreCase("all"))
+                return false;
+            
+            int healed = 0;
+            for(Player player : Bukkit.getOnlinePlayers()) {
+                if(player.hasPermission(args[1])) {
+                    player.setHealth(20);
+                    player.sendMessage(Language.getTranslationForSender(sender, "healedBy", sender.getName()));
+                    healed++;
+                }
+            }
+            sender.sendMessage(Language.getTranslationForSender(sender, "healOthersByPerm", healed + "", args[1]));
+        }
+        
+        return true;
+    }
+
+}
