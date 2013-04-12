3062768105d36e8c517f1ea2f532f3dcf51fb593
Keir Nellyer <keir@nellyer.co.uk>
2013-04-12 13:39:20 +0100

    Add javassist as a library, this will fix our hacky reflection stuff

--- src/com/commandsex/CommandsEX.java
+++ src/com/commandsex/CommandsEX.java
@@ -93,6 +93,7 @@ public class CommandsEX extends JavaPlugin {
         Language.init();
 
         Libraries.registerLibrary("reflections-0.9.9-RC1", "http://www.commandsex.com/downloads/dependencies/reflections-0.9.9-RC1.jar");
+        Libraries.registerLibrary("javassist", "http://www.commandsex.com/downloads/dependencies/javassist.jar");
         Libraries.registerLibrary("metrics-R6", "http://www.commandsex.com/downloads/dependencies/metrics-R6.jar");
         Libraries.registerLibrary("jooq-3.0.0-RC2", "http://www.commandsex.com/downloads/dependencies/jooq-3.0.0-RC2.jar");
 
