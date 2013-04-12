ce3b0befe9966cb74766d71b29923f7d0aa9c40c
Kezz101 <1millionchances@gmail.com>
2013-04-12 12:16:56 +0100

    Added function names so @iKeirNez can code :3

--- src/com/commandsex/helpers/plugman/BukgetPlugin.java
+++ src/com/commandsex/helpers/plugman/BukgetPlugin.java
@@ -1,7 +1,73 @@
 package com.commandsex.helpers.plugman;
 
+import java.net.URL;
+
 public class BukgetPlugin {
 
+    private BukgetPlugin() {
 
+    }
+
+    public static BukgetPlugin getPluginFromList(BukgetPluginList list, String name) {
+        return new BukgetPlugin();
+    }
+
+    public URL download() {
+        return null;
+    }
+
+    public URL devPage() {
+        return null;
+    }
+
+    public int downloads() {
+        return 0;
+    }
+
+    public String version() {
+        return null;
+    }
+
+    public String bukkitVersion() {
+        return null;
+    }
+
+    public String name() {
+        return null;
+    }
+
+    public String description() {
+        return null;
+    }
+
+    public String[] authors() {
+        return null;
+    }
+
+    public BukgetPluginList.Category[] categories() {
+        return null;
+    }
+
+    public String[] dependenciesSoft() {
+        return null;
+    }
+
+    public String[] dependenciesHard() {
+        return null;
+    }
+
+    public Stage stage() {
+        return null;
+    }
+
+    public enum Stage {
+        PLANNING,
+        ALPHA,
+        BETA,
+        MATURE,
+        INACTIVE,
+        ABANDONED,
+        DELETED
+    }
 
 }
--- src/com/commandsex/helpers/plugman/BukgetPluginList.java
+++ src/com/commandsex/helpers/plugman/BukgetPluginList.java
@@ -1,7 +1,37 @@
 package com.commandsex.helpers.plugman;
 
+import java.util.Map;
+
 public class BukgetPluginList {
 
+    private BukgetPluginList() {
 
+    }
+
+    public static BukgetPluginList getListFromQuery(String searchTerm, Category category) {
+        return new BukgetPluginList();
+    }
+
+    public Map<Integer, String> getPlugins() {
+        return null;
+    }
+
+    public enum Category {
+        ADMIN_TOOLS,
+        DEVELOPER_TOOLS,
+        FUN,
+        MECHANICS,
+        TELEPORTATION,
+        WORLD_GENRATORS,
+        ANTI_GRIEFING_TOOLS,
+        ECONOMY,
+        GENERAL,
+        WEBSITE_ADMINISTRATION,
+        CHAT_RELATED,
+        FIXES,
+        INFORMATIONAL,
+        ROLE_PLAYING,
+        WORLD_EDITING_AND_MANAGEMENT
+    }
 
 }
