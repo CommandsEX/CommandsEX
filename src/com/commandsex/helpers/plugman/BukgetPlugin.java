6a12f3ebc3ecda1751453e7202d251a5cd67603e
Kezz101 <1millionchances@gmail.com>
2013-04-12 13:23:47 +0100

    Made realistic

--- src/com/commandsex/helpers/plugman/BukgetPlugin.java
+++ src/com/commandsex/helpers/plugman/BukgetPlugin.java
@@ -5,80 +5,20 @@ import java.net.URL;
 
 public class BukgetPlugin {
 
-    private BukgetPlugin() {}
-
-    public static BukgetPlugin getPluginFromList(BukgetPluginList list, String name) throws IOException {
-        return new BukgetPlugin();
-    }
-
-    public static BukgetPlugin getPluginBySlug(String slug) throws IOException {
-         return null;
-    }
-
-    public static URL getDownloadLinkFromSlug(String slug) throws IOException {
+    public Object getFieldValue(Bukget.Field field) {
         return null;
     }
 
-    public URL download() {
+    /*
+     * START STATIC METHODS
+     */
+
+    public static BukgetPlugin getPluginFromSlug(String slug, Bukget.Version version, Bukget.Field... toReturn) throws IOException {
         return null;
     }
 
-    public URL devPage() {
+    public static BukgetPlugin getPluginFromList(BukgetPluginList list, int id, Bukget.Version version, Bukget.Field... toReturn) throws IOException {
         return null;
-    }
-
-    public int downloads() {
-        return 0;
-    }
-
-    public String version() {
-        return null;
-    }
-
-    public String bukkitVersion() {
-        return null;
-    }
-
-    public String name() {
-        return null;
-    }
-
-    public String description() {
-        return null;
-    }
-
-    public String changelog() {
-        return null;
-    }
-
-    public String[] authors() {
-        return null;
-    }
-
-    public BukgetPluginList.Category[] categories() {
-        return null;
-    }
-
-    public String[] dependenciesSoft() {
-        return null;
-    }
-
-    public String[] dependenciesHard() {
-        return null;
-    }
-
-    public Stage stage() {
-        return null;
-    }
-
-    public enum Stage {
-        PLANNING,
-        ALPHA,
-        BETA,
-        MATURE,
-        INACTIVE,
-        ABANDONED,
-        DELETED
     }
 
 }
--- src/com/commandsex/helpers/plugman/BukgetPluginList.java
+++ src/com/commandsex/helpers/plugman/BukgetPluginList.java
@@ -1,38 +1,39 @@
 package com.commandsex.helpers.plugman;
 
+import com.commandsex.helpers.WebHelper;
+import com.google.gson.JsonParser;
+import org.json.simple.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class BukgetPluginList {
+    private String[][] arr;
 
-    private BukgetPluginList(Map<Integer, String> plugins) {
-
+    private BukgetPluginList(String[][] plugins) {
+        this.arr = plugins;
     }
 
-    public static BukgetPluginList getListFromQuery(String searchTerm, Category category) throws IOException {
-        return new BukgetPluginList(null);
-    }
-
-    public Map<Integer, String> getPlugins() {
+    public Map<Integer, String> getIDNameMap() {
+        Map<Integer, String> returnMe = new HashMap<>();
         return null;
     }
 
-    public enum Category {
-        ADMIN_TOOLS,
-        DEVELOPER_TOOLS,
-        FUN,
-        MECHANICS,
-        TELEPORTATION,
-        WORLD_GENRATORS,
-        ANTI_GRIEFING_TOOLS,
-        ECONOMY,
-        GENERAL,
-        WEBSITE_ADMINISTRATION,
-        CHAT_RELATED,
-        FIXES,
-        INFORMATIONAL,
-        ROLE_PLAYING,
-        WORLD_EDITING_AND_MANAGEMENT
+    public String getDescriptionFromID(int id) {
+        return null;
+    }
+
+    public String getSlugFromID(int id) {
+        return null;
+    }
+
+    /*
+     * START STATIC METHODS
+     */
+
+    public static BukgetPluginList getFromQuery(int results, Bukget.Field field, Bukget.SearchAction action, String value, int amount, Bukget.Field toReturn) throws IOException {
+        return null;
     }
 
 }
