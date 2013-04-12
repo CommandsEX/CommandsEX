6a27a71e480e81238a5694018939f2b81f122e23
Kezz101 <1millionchances@gmail.com>
2013-04-12 16:03:22 +0100

    Added method content and Javadocs

--- src/com/commandsex/helpers/plugman/Bukget.java
+++ src/com/commandsex/helpers/plugman/Bukget.java
@@ -1,7 +1,16 @@
 package com.commandsex.helpers.plugman;
 
+import org.apache.commons.lang.WordUtils;
+
+/**
+ * Holder of all Bukget related enums
+ * @author Kezz101
+ */
 public class Bukget {
 
+    /**
+     * Enum containing all possible Categories a plugin could be
+     */
     public enum Category {
         ADMIN_TOOLS("Admin Tools"),
         DEVELOPER_TOOLS("Developer Tools"),
@@ -25,18 +34,35 @@ public class Bukget {
             this.slug = slug;
         }
 
+        /**
+         * Gets the Bukget API friendly version of the category
+         */
         @Override
-        public String toString() { return slug; }
+        public String toString() { return slug.replace(" ", "%20"); }
 
+        /**
+         * Gets the user friendly name of the category
+         */
+        public String getFriendlyName() { return slug; }
+
+        /**
+         * Gets a category from an entered string
+         * @param category the string
+         * @return the category or <code>null</code> if not found
+         */
         public static Category getFromString(String category) {
             for(Category cat : Category.values()) {
-                if(cat.toString().equalsIgnoreCase(category))
+                if(cat.getFriendlyName().equalsIgnoreCase(category))
                     return cat;
             }
             return null;
         }
+
     }
 
+    /**
+     * Enum containing all possible plugin versions
+     */
     public enum Version {
         LATEST("latest"),
         LATEST_STABLE_RELEASE("release"),
@@ -49,27 +75,56 @@ public class Bukget {
             this.version = version;
         }
 
+        /**
+         * Gets the Bukget API friendly version of the version
+         */
         @Override
         public String toString() { return version; }
+
+        /**
+         * Gets the user friendly version of the version
+         */
+        public String getFriendlyName() { return WordUtils.capitalize(toString()); }
+
+        /**
+         * Gets a version from an entered string
+         * @param version the string
+         * @return the version or <code>null</code> if not found
+         */
+        public static Version getFromString(String version) {
+            for(Version cat : Version.values()) {
+                if(cat.getFriendlyName().equalsIgnoreCase(version))
+                    return cat;
+            }
+            return null;
+        }
+
     }
 
+    /**
+     * Enum containing all retrievable and searchable plugin fields. <br />
+     * <b>Note:</b> if searching, fields can be negated (e.g don't include foo) using <code>field.negate()</code>
+     */
     public enum Field {
         SLUG("slug"),
         PLUGIN_NAME("plugin_name"),
         CATEGORIES("categories"),
         AUTHORS("authors"),
-        WEBSITE("webpage"),
+        WEBSITE("website"),
         BUKKIT_DEV_PAGE("dbo_page"),
         DESCRIPTION("description"),
         VERSION("versions.version"),
         FILE_LINK("versions.link"),
         DOWNLOAD_LINK("versions.downloads"),
-        BUKKIT_VERSION("versions.status"),
+        BUKKIT_VERSION("versions.game_versions"),
         CHANGELOG("versions.changelog"),
         DATE_RELEASED("versions.date"),
         VERSION_SLUG("versions.slug"),
         HARD_DEPENDENCIES("versions.hard_dependencies"),
-        SOFT_DEPENDENCIES("versions.soft_dependencies");
+        SOFT_DEPENDENCIES("versions.soft_dependencies"),
+        POPULARITY_DAILY("popularity.daily"),
+        POPULARITY_WEEKLY("popularity.weekly"),
+        POPULARITY_MONTHLY("popularity.monthly");
 
         private String field;
 
@@ -77,13 +132,46 @@ public class Bukget {
             this.field = field;
         }
 
+        /**
+         * Gets the Bukget API friendly version of the field
+         */
         @Override
         public String toString() { return field; }
 
+        /**
+         * Returns the negated Bukget API friendly version of the field
+         */
         public String negate() { return "-" + field; }
+
+        /**
+         * Returns a  user friendly version of the field
+         */
+        public String getFriendlyName() {
+            if(field.startsWith("versions."))
+                return WordUtils.capitalize(field.replace("versions.", "").replace("_", " "));
+            if(field.startsWith("popularity."))
+                return WordUtils.capitalize(field.replace("popularity.", "")) + "Popularity";
+            return WordUtils.capitalize(field.replace("_", " ").replace("dbo", "bukkit dev"));
+        }
+
+        /**
+         * Gets a field from an entered string. <br />
+         * This will match the entered string to the user friendly name
+         * @param field the string
+         * @return the field or <code>null</code> if not found
+         */
+        public static Field getFromString(String field) {
+            for(Field f : Field.values())
+                if(f.getFriendlyName().equalsIgnoreCase(field))
+                    return f;
+            return null;
+        }
 
     }
 
+    /**
+     * Enum containing all available actions for use in Bukget searches
+     */
     public enum SearchAction {
         EQUALS("="),
         NOT_EQUAL("!="),
@@ -99,8 +187,23 @@ public class Bukget {
             this.action = action;
         }
 
+        /**
+         * Returns the Bukget API and user friendly version of the action
+         */
         @Override
         public String toString() { return action; }
+
+        /**
+         * Gets an action from an entered string
+         * @param action the string
+         * @return the action or <code>null</code> if not found
+         */
+        public static SearchAction getFromString(String action) {
+            for(SearchAction a : SearchAction.values())
+                if(action.toString().equalsIgnoreCase(action))
+                    return a;
+            return null;
+        }
 
     }
 
